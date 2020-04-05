a4f79e7c350d55e8e29298132d52abe56243a0aa
ashish kumar singh <ashishkumarsinghh@outlook.com>
2020-04-05 16:33:59 +0530

    Feature: Make CourseInfo, CourseCategory, Note as Data Classes #4

--- app/src/main/java/com/example/letslearn/CourseInfo.kt
+++ app/src/main/java/com/example/letslearn/CourseInfo.kt
@@ -1,19 +1,16 @@
 package com.example.letslearn
 
-class CourseInfo(val courseID: String, var courseTitle: String, var courseDesc: String, var instructors: String, var category: String) {
+data class CourseInfo(val courseID: String, var courseTitle: String, var courseDesc: String, var instructors: String, var category: String) {
     override fun toString(): String {
         return courseTitle
     }
 }
 
-class CourseCategory(val categoryId: String, var categoryTitle: String) {
+data class CourseCategory(val categoryId: String, var categoryTitle: String) {
     override fun toString(): String {
         return categoryTitle
     }
 }
 
-class Note(var noteTitle: String, var note: String) {
-    override fun toString(): String {
-        return noteTitle
-    }
+data class Note(var noteTitle: String, var note: String) {
 }
