fb7639d51b0747572f44d223556d4db0bcd75023
ashish kumar singh <ashishkumarsinghh@outlook.com>
2020-04-05 11:10:00 +0530

    Fix Code readability.

--- app/src/main/java/com/example/letslearn/MainActivity.kt
+++ app/src/main/java/com/example/letslearn/MainActivity.kt
@@ -6,57 +6,51 @@ import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.course.*
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var dm : DataManager
+    private val tag = "LetsLearnMainActivity"
+    private lateinit var dm: DataManager
     private lateinit var setCategory: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         dm = DataManager()
-        addCatSpnrAdptr()
+        addCategorySpinner()
     }
 
-    private fun addCatSpnrAdptr(){
-        spnrCategory.adapter = ArrayAdapter(this,R.layout.category, R.id.tvCategoryItem, dm.categories.toArray())
-        spnrCategory.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+    private fun addCategorySpinner() {
+        spnrCategory.adapter = ArrayAdapter(
+            this, R.layout.category,
+            R.id.tvCategoryItem, dm.categories.toArray()
+        )
+        spnrCategory.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 TODO("Not yet implemented")
             }
-
             override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long
             ) {
                 setCategory = dm.categories[id.toInt()].categoryId
-                Log.d("letslearn", setCategory)
-                addCourSpnrAdptr()
+                Log.d(tag, setCategory)
+                addCourSpinnerAdapter()
             }
-
         }
     }
 
-    private fun addCourSpnrAdptr(){
-        var courseArr = dm.courses.filter { course -> course.category==setCategory }
+    private fun addCourSpinnerAdapter() {
+        val courseArr = dm.courses.filter { course -> course.category == setCategory }
         spnrCourse.adapter = ArrayAdapter(this, R.layout.course, R.id.tvCourseItem, courseArr)
-        spnrCourse.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+        spnrCourse.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 TODO("Not yet implemented")
             }
-
             override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long
             ) {
-                Log.d("ashish", id.toString())
-                Log.d("ashish", courseArr[id.toInt()].courseTitle)
+                Log.d(tag, id.toString())
+                Log.d(tag, courseArr[id.toInt()].courseTitle)
             }
         }
     }
